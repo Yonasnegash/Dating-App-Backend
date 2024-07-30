8295ec7009532753e3948f0bd8751350bd0d20de
Yonas Negash <yonas.negash91@gmail.com>
2024-07-30 18:37:49 +0300

    Revert "done user registration, profile completion"
    This reverts commit 0b108f2189d72e9eba137443ddbca3e10de0c47b.

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-require("dotenv").config();
-
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const connectDB = require("./src/config/db");
-const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-
-connectDB();
-
-app.use("/api/auth", authRoutes);
 
 module.exports = app;
