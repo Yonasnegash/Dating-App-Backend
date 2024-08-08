a3c139183536b2a235218b497585e894634623d2
Yonas Negash <yonas.negash91@gmail.com>
2024-08-08 12:17:06 +0300

    Debug - add a temporary route to list files in the uploads

--- app.js
+++ app.js
@@ -26,4 +26,29 @@ app.use("/api/liking", dislikeRoutes);
 // Serve static files from the uploads directory
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
+app.get("/list-uploads-one", (req, res) => {
+  const fs = require("fs");
+  const uploadDir = path.join(__dirname, "..", "uploads");
+
+  fs.readdir(uploadDir, (err, files) => {
+    if (err) {
+      res.status(500).send("Unable to read uploads directory");
+    } else {
+      res.json(files);
+    }
+  });
+});
+
+app.get("/list-uploads-two", (req, res) => {
+  const fs = require("fs");
+  const uploadDir = path.join(__dirname, "uploads");
+
+  fs.readdir(uploadDir, (err, files) => {
+    if (err) {
+      res.status(500).send("Unable to read uploads directory");
+    } else {
+      res.json(files);
+    }
+  });
+});
 module.exports = app;
