6264d6ab9df71634186675651a828928c0690ff0
Yonas Negash <yonas.negash91@gmail.com>
2024-08-08 10:46:55 +0300

    create upload path if not exist

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,11 +1,19 @@
-// src/config/multerConfig.js
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 // Set up storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "..", "uploads")); // specify the upload directory
+    const uploadPath = path.join(__dirname, "..", "uploads");
+
+    // Check if the directory exists
+    if (!fs.existsSync(uploadPath)) {
+      // Create the directory if it does not exist
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath); // specify the upload directory
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)); // append timestamp to the original file name
